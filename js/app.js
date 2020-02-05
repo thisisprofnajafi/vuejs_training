c2ab8dc72d8ec6b799a1ab8b352682143f585516
F.Golrang <mahtabgole78@gmail.com>
2020-02-05 16:47:47 +0330

    simple stuff are learned and this is events and modifres

---
+++ js/app.js
@@ -0,0 +1,25 @@
+new Vue({
+
+    el:'#vue-app',
+    data:{
+        age: 24,
+        x:0,
+        y:0
+    },
+    methods:{
+        add:function(inc){
+            this.age+=inc
+        },
+        subtract:function(dec){
+            this.age-= dec;
+        }
+        ,updateXY:function(){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click:function(){
+            alert("Are you ready to buy some shit?");
+        }
+    }
+
+});
