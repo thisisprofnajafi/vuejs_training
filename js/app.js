d147087376198f3f46ada67b4be5b468a99f4343
F.Golrang <mahtabgole78@gmail.com>
2020-02-10 16:57:46 +0330

    conditions

--- js/app.js
+++ js/app.js
@@ -1,31 +1,15 @@
 new Vue({
 
-    el:'#vue-app',
-    data:{
-        age: 24,
-        x:0,
-        y:0
+    el: '#vue-app',
+    data: {
+        error: false,
+        success: false
     },
-    methods:{
-        add:function(inc){
-            this.age+=inc
-        },
-        subtract:function(dec){
-            this.age-= dec;
-        }
-        ,updateXY:function(){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click:function(){
-            alert("Are you ready to buy some shit?");
-        }
-        ,logAge:function(){
-            console.log("you insert your age");
-        },
-        logAge:function(){
-            console.log("you insert your name");
-        }
+    methods: {
+
+    },
+    computed: {
+
     }
 
 });
