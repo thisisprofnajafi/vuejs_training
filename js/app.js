e31eee2a3b8dbc1961babbd909bdcf13f3c3f89e
F.Golrang <mahtabgole78@gmail.com>
2020-02-10 17:15:27 +0330

    loops and for

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ new Vue({
 
     el: '#vue-app',
     data: {
-        error: false,
-        success: false
+       chars: ['Mario' , 'Luigi' , 'Yoshi' , 'Browser'],
+       ninjas:[
+           {name:'abi',age:24},
+           {name:'ali',age:23},
+           {name:'naghi',age:25}
+       ]
     },
     methods: {
 
