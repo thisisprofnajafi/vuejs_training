17a097d100116077d1350d44d0919d570af32079
F.Golrang <mahtabgole78@gmail.com>
2020-02-10 22:07:04 +0330

    refs

--- js/app.js
+++ js/app.js
@@ -1,52 +1,17 @@
 
-Vue.component('greeting',{
-
-    template: '<p>Hey petter, I am a {{name}} <button v-on:click="changeName">How are you?</button></p>',
-    data :function(){
-        return {
-            name:'yousha'
-        }
-    },
-    methods:{
-        changeName: function(){
-            this.name = 'Maria'
-        }
-    }
-
-});
-
 
 var one = new Vue({
-
-    el: '#vue-app-one',
+    el: '#vue-app',
     data: {
-        title:"view one"
+        output:'Your Fav Food'
     },
     methods: {
-       
-    },
-    computed: {
-        greet:function(){
-            return "Hello from app one"
-        }
+       readRefs:function(){
+        this.output = this.$refs.input.value
+        console.log(this.$refs.test.innerHTML)
+       }
     }
+   
 
 });
-var two = new Vue({
 
-    el: '#vue-app-two',
-    data: {
-        title:"view two"
-    },
-    methods: {
-        changeTitle: function(){
-            one.title = "This is changed by the second vue app"
-        }
-    },
-    computed: {
-         greet:function(){
-            return "Hi dude this is app two"
-        }
-    }
-
-})
