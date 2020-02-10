151293c1e282875d55abef1ce352b16b4e1395c0
F.Golrang <mahtabgole78@gmail.com>
2020-02-10 21:35:56 +0330

    multi vue

--- js/app.js
+++ js/app.js
@@ -1,24 +1,35 @@
-new Vue({
+var one = new Vue({
 
-    el: '#vue-app',
+    el: '#vue-app-one',
     data: {
-        health: 100,
-        ended: false
+        title:"view one"
     },
     methods: {
-        punch: function () {
-            this.health -= 10
-            if (this.health <= 0) {
-                this.ended = true;
-            }
-        },
-        restart: function () {
-            this.health = 100;
-            this.ended = false;
-        }
+       
     },
     computed: {
-
+        greet:function(){
+            return "Hello from app one"
+        }
     }
 
 });
+var two = new Vue({
+
+    el: '#vue-app-two',
+    data: {
+        title:"view two"
+    },
+    methods: {
+        changeTitle: function(){
+            one.title = "This is changed by the second vue app"
+        }
+    },
+    computed: {
+         greet:function(){
+            return "Hi dude this is app two"
+        }
+    }
+
+});
+
